102479fc62aa0c74424b016b49cbe9159a9d1386
Renan Biagiotti <renan.biagiotti22@gmail.com>
2020-02-25 04:25:56 -0300

    Adding CrossOrigin

--- src/main/java/br/com/reservation/court/controller/CourtController.java
+++ src/main/java/br/com/reservation/court/controller/CourtController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,6 +53,7 @@ public class CourtController {
 		return new ResponseEntity<>(allCourts, HttpStatus.OK);
 	}
 	
+	@CrossOrigin("*")
 	@GetMapping(value="retrieveCourtByNumber/{courtNumber}")
 	public ResponseEntity<?> retrieveCourtByNumber(@PathVariable int courtNumber){
 		
